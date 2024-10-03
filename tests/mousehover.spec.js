f324f6edf1ef3bac1ad9cf68c2b8f5f3c9a23e46
Rajat Kumar Pradhan <kumarrajatpradhan5537@gmail.com>
2024-10-03 11:01:30 +0530

    trying to push

---
+++ tests/mousehover.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+
+test('Mouse hover', async ({ page }) => {
+
+    /* await page.goto('https://freelance-learn-automation.vercel.app/login')
+     await page.getByPlaceholder('Enter Email').fill('admin@email.com')
+     await page.getByPlaceholder('Enter Password').fill('admin@123')
+     await page.getByRole('button',{name: "Sign in"}).click()
+ 
+     await page.locator("//span[text()='Manage']").hover()
+     await page.locator("//a[normalize-space()='Manage Courses']").click()
+ */
+    await page.goto('https://www.google.com/')
+
+
+})
